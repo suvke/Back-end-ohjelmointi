6969fd88524f9129c52ea745e65ed78f1fc4f899
Suvi Keränen <suvi.keranen@outlook.com>
2023-08-27 09:10:01 +0300

    First commit

---
+++ src/main/java/s23/carDatabase/web/CarController.java
@@ -0,0 +1,48 @@
+package s23.carDatabase.web;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CarController {
+	
+	@RequestMapping("main")
+	@ResponseBody
+	public String showMainPage() {
+		return "Tämä on etusivu";
+	}
+	
+	@RequestMapping("moi")
+	@ResponseBody
+	public String sayHello(@RequestParam (name="nimesi", required=false, defaultValue="Muumi") String nimi) {
+		return ("hei " + nimi);
+	}
+	
+	@RequestMapping("moikka")
+	@ResponseBody
+	public String sayHello2(@RequestParam (name="nimesi", required=false, defaultValue="Muumi") String nimi, 
+			@RequestParam(name="ika") int age) {
+		return ("hei " + nimi + " " + age + " vuotta.");
+	}
+	
+	@RequestMapping("index")
+	@ResponseBody
+	public String mainPage() {
+		return "This is the main page";
+	}
+	
+	@RequestMapping("contact")
+	@ResponseBody
+	public String contactPage() {
+		return "This is the contact page";
+	}
+	
+	@RequestMapping("hello")
+	@ResponseBody
+	public String greeting(@RequestParam (name="location", required=false, defaultValue="moon") String sijainti, 
+			@RequestParam (name="name", required=false, defaultValue="Muumi") String nimi) {
+		return "Welcome to the " + sijainti + " " + nimi + "!";
+	}
+	
+}
---
+++ src/test/java/com/example/Back/end/ohjelmointikurssi/BackEndOhjelmointikurssiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Back.end.ohjelmointikurssi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndOhjelmointikurssiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
